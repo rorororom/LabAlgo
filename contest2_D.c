57969508685bb5e2a532af23f40a28e9f127153d
rorororom <mariia.romashkina.mih@gmail.com>
2024-04-19 00:09:32 +0300

    переделана задача

--- contest2_D.c
+++ contest2_D.c
@@ -1,15 +1,21 @@
-#include <limits.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <assert.h>
 #include <stdbool.h>
-#include <math.h>
 #include <time.h>
 #include <string.h>
 
 struct node {
     int value;
     int flag_zero;
+};
+
+struct HashTable {
+    struct arrHash2* table;
+    int size;
+    int A;
+    int B;
+    int P;
 };
 
 struct arrHash2 {
@@ -20,35 +26,14 @@ struct arrHash2 {
     int size;
 };
 
-bool areCoprime(int a, int b) {
-    while (b != 0) {
-        int temp = b;
-        b = a % b;
-        a = temp;
-    }
-    return a == 1;
-}
+typedef int status_t;
 
-void find(int** table, int value);
+enum STATUS {
+    OK,
+    ERROR
+};
 
-
-int generatePrime(int min, int max) {
-    for (;;) {
-        int num = rand() % (max - min + 1) + min;
-        bool isPrime = true;
-        for (int i = 2; i * i <= num; i++) {
-            if (num % i == 0) {
-                isPrime = false;
-                break;
-            }
-        }
-        if (isPrime) {
-            return num;
-        }
-    }
-}
-
-int nearestLowerPowerOfTwo(int N) {
+int GenerSizeHT(int N) {
     int power = 1;
     while (power <= N) {
         power *= 2;
@@ -56,227 +41,241 @@ int nearestLowerPowerOfTwo(int N) {
     return power;
 }
 
-int generateA(int size) {
-    int A;
-    do {
-        A = rand();
-        A += (A % 2 == 0) ? 1 :  0;
+status_t ReadData(int** arr, int* N, bool* flagZero) {
+    int res = scanf("%d", N);
+    if (res != 1) {
+        printf("не считался размер\n");
+        return ERROR;
+    }
 
-    } while (!areCoprime(A, size));
-    return A;
+    *arr = (int*)calloc(*N, sizeof(int));
+
+    for (size_t i = 0; i < *N; i++) {
+        int res = scanf("%d", &((*arr)[i]));
+        if (res != 1) {
+            printf("не считался размер\n");
+            return ERROR;
+        }
+
+        if((*arr)[i] == 0) {
+            *flagZero = true;
+        }
+    }
+
+    return OK;
 }
 
+void HF_GenerCoeff(int* A, int* B, int* P, size_t size) {
+    *A = rand() % size;
+    *A = 2 * (*A) + 1;
+    *B = rand() % size;
+    *P = rand() % size + size;
+    *P = 2 * (*P) + 1;
+}
+
+struct HashTable* HT_Ctor(size_t N) {
+    struct HashTable* hashTable = (struct HashTable*)calloc(1, sizeof(struct HashTable));
+
+    int size = GenerSizeHT(N);
+
+    hashTable->size = size;
+
+    HF_GenerCoeff(&hashTable->A, &hashTable->B, &hashTable->P, hashTable->size);
+    return hashTable;
+}
+
+struct HashTable* HT_collizion(struct HashTable* HT, int* collizion) {
+    struct HashTable* ht_collizion = (struct HashTable*)calloc(1, sizeof(struct HashTable));
+    assert(ht_collizion);
+
+    ht_collizion->table = (struct arrHash2*)calloc(HT->size, sizeof(struct arrHash2));
+    assert(ht_collizion->table);
+
+    for (size_t i = 0; i < HT->size; i++) {
+        if (collizion[i] != 0) {
+            ht_collizion->table[i].arr = (struct node*)calloc(collizion[i], sizeof(struct node));
+            assert(ht_collizion->table[i].arr);
+        }
+
+        ht_collizion->table[i].size = 0;
+    }
+
+    ht_collizion->size = HT->size;
+    ht_collizion->A = HT->A;
+    ht_collizion->B = HT->B;
+    ht_collizion->P = HT->P;
+
+    return ht_collizion;
+}
+
+void CalculateCollisions(int* arr, int** collizion, int N, struct HashTable* HT) {
+    while (true) {
+        for (int i = 0; i < N; i++) {
+            int key = arr[i];
+            if (key == 0)
+                continue;
+            int hashValue = ((HT->A * key + HT->B) % HT->P + HT->P) % HT->P % HT->size;
+            (*collizion)[hashValue]++;
+        }
+
+        int sum = 0;
+        for (int i = 0; i < HT->size; i++) {
+            sum += (*collizion)[i] * (*collizion)[i];
+        }
+
+        if (sum < 4 * N) {
+            break;
+        }
+
+        for (int i = 0; i < HT->size; i++) {
+            (*collizion)[i] = 0;
+        }
+
+        HF_GenerCoeff(&HT->A, &HT->B, &HT->P, HT->size);
+    }
+}
+
+void UpdateCollizion(struct HashTable* ht_collizion, int* arr, int N, int* collizion) {
+    for (int i = 0; i < N; i++) {
+        int key = arr[i];
+        if (key == 0)
+            continue;
+        int hashValue = ((ht_collizion->A * key + ht_collizion->B) % ht_collizion->P + ht_collizion->P) % ht_collizion->P % ht_collizion->size;
+        ht_collizion->table[hashValue].arr[ht_collizion->table[hashValue].size].value = key;
+        ht_collizion->table[hashValue].size++;
+    }
+}
+
+int HashFunc(struct arrHash2* ht, int key) {
+    return ((ht->A * key + ht->B) % ht->P + ht->P) % ht->P % ht->size;
+}
+
+void UpdateHT(struct HashTable* HT, int* collizion) {
+    HT->table = (struct arrHash2*)calloc(HT->size, sizeof(struct arrHash2));
+    for (size_t i = 0; i < HT->size; i++) {
+        HT->table[i].size = collizion[i] * collizion[i];
+        if (HT->table[i].size == 0) {
+            continue;
+        }
+        HT->table[i].arr = (struct node*)calloc(HT->table[i].size, sizeof(struct node));
+        assert(HT->table[i].arr);
+    }
+}
+
+void BuildCorrectHashFunc2(struct HashTable* HT, struct HashTable* HT_collizion, int* arr) {
+    for (size_t i = 0; i < HT->size; i++) {
+        if (HT->table[i].size > 0) {
+            HF_GenerCoeff(&HT->table[i].A, &HT->table[i].B, &HT->table[i].P, HT->table[i].size);
+        }
+
+        struct node* temp_arr = (struct node*)calloc(HT->table[i].size, sizeof(struct node));
+
+        for (int j = 0; j < HT_collizion->table[i].size; j++) {
+            int key = HT_collizion->table[i].arr[j].value;
+            if (key == 0)
+                continue;
+
+            int hashValue = ((HT->table[i].A * key + HT->table[i].B) % HT->table[i].P + HT->table[i].P) % HT->table[i].P % HT->size;
+
+            if (temp_arr[hashValue].value != 0) {
+                HF_GenerCoeff(&HT->table[i].A, &HT->table[i].B, &HT->table[i].P, HT->table[i].size);
+                for (size_t k = 0; k < HT->table[i].size; k++) {
+                    temp_arr[k].value = 0;
+                }
+
+                j = -1;
+            } else {
+                temp_arr[hashValue].value = key;
+            }
+        }
+
+        for (size_t j = 0; j < HT->table[i].size; j++) {
+            HT->table[i].arr[j].value = temp_arr[j].value;
+        }
+
+        free(temp_arr);
+    }
+}
+
+void ProcessCommands(struct HashTable* HT, bool flagZero) {
+    char command[10];
+    int res = scanf("%s", command);
+    if (res != 1) {
+        printf("не считался размер\n");
+        return;
+    }
+
+    while (command[0] != '.') {
+        int value = atoi(command);
+        if (value == 0) {
+            if(flagZero)
+                printf("YES\n");
+            else
+                printf("NO\n");
+
+            int res = scanf("%s", command);
+            if (res != 1) {
+                printf("не считался размер\n");
+                return;
+            }
+            continue;
+        }
+
+        int hash1 = ((HT->A * value + HT->B) % HT->P + HT->P) % HT->P % HT->size;
+        if (HT->table[hash1].arr == NULL) {
+            printf("NO\n");
+
+            int res = scanf("%s", command);
+            if (res != 1) {
+                printf("не считался размер\n");
+                return;
+            }
+
+            continue;
+        }
+
+        int hash2 = HashFunc(&HT->table[hash1], value);
+        if (HT->table[hash1].arr[hash2].value == value) {
+            printf("YES\n");
+        } else {
+            printf("NO\n");
+        }
+
+        scanf("%s", command);
+    }
+}
 
 int main() {
     srand(time(NULL));
 
     int N = 0;
-    scanf("%d", &N);
-    bool nul = false;
-    int* arr = (int*)calloc(N, sizeof(int));
-    for (size_t i = 0; i < N; i++) {
-        scanf("%d", &arr[i]);
-        if(arr[i] == 0)
-        {
-            nul = true;
-        }
-    }
+    int* arr;
 
-    int size = nearestLowerPowerOfTwo(N);
+    bool flagZero = false;
 
-    int A, B, P;
-    A = rand() % size;
-    A = 2 * A + 1;
-    B = rand() % size;
-    P = rand() % size + size;
-    P = 2 * P + 1;
+    status_t res = ReadData(&arr, &N, &flagZero);
+    if (res)
+        return 1;
 
-    // printf("Obtained parameters: A = %d, B = %d, P = %d\n", A, B, P);
+    struct HashTable* HT = HT_Ctor(N);
 
-    struct arrHash2* hashTable = (struct arrHash2*)malloc(size * sizeof(struct arrHash2));
-    assert(hashTable);
+    int* collizion = (int*)calloc(HT->size, sizeof(int));
+    assert(collizion);
 
-    int* collizion = (int*)calloc(size, sizeof(int));
+    CalculateCollisions(arr, &collizion, N, HT);
 
-    while (true) {
-        for (int i = 0; i < N; i++) {
-        int key = arr[i];
-        if (key == 0)
-            continue;
-        int hashValue = ((A * key + B) % P + P) % P % size;
-        collizion[hashValue]++;
-        // printf("Hash value for key %d: %d\n", key, hashValue);
-    }
+    struct HashTable* ht_collizion = HT_collizion(HT, collizion);
 
-        int sum = 0;
-        for (int i = 0; i < size; i++) {
-            sum += collizion[i] * collizion[i];
-        }
+    UpdateCollizion(ht_collizion, arr, N, collizion);
 
-//         for (int i = 0; i < size; i++) {
-//             printf("%d %d\n",i,  collizion[i]);
-//         }
-//         printf("\n");
-//
-//         printf("!! %d - %d\n", sum, 4 * N);
-        if (sum < 4 * N) {
-            break;
-        }
+    UpdateHT(HT, collizion);
 
-        for (int i = 0; i < size; i++) {
-            collizion[i] = 0;
-        }
+    BuildCorrectHashFunc2(HT, ht_collizion, arr);
 
-        A = rand() % size;
-        A = 2 * A + 1;
-        B = rand() % size;
-        P = rand() % size + size;
-        P = 2 * P + 1;
+    ProcessCommands(HT, flagZero);
 
-        // printf("Obtained parameters: A = %d, B = %d, P = %d\n", A, B, P);
-    }
-
-    struct arrHash2* AAA = (struct arrHash2*)malloc(size * sizeof(struct arrHash2));
-    for (int i = 0; i < size; i++) {
-        AAA[i].arr = (struct node*)calloc(collizion[i], sizeof(struct node));
-        AAA[i].size = 0;
-    }
-
-    for (int i = 0; i < N; i++) {
-        int key = arr[i];
-        if (key == 0)
-            continue;
-        int hashValue = ((A * key + B) % P + P) % P % size;
-        AAA[hashValue].arr[AAA[hashValue].size++].value = key;
-        // printf("Hash value for key %d: %d\n", key, hashValue);
-    }
-
-    // for (int i = 0; i < size; i++) {
-    //     printf("%d ", i);
-    //     for (int j = 0; j < AAA[i].size; j++) {
-    //         printf("%d ", AAA[i].arr[j].value);
-    //     }
-    //     printf("\n");
-    // }
-
-    // printf(":1\n");
-    for (int i = 0; i < size; i++) {
-        hashTable[i].size = collizion[i] * collizion[i];
-        if (hashTable[i].size == 0) {
-            continue;
-        }
-        hashTable[i].arr = (struct node*)calloc(hashTable[i].size, sizeof(struct node));
-
-        // do {
-        //     hashTable[i].A = rand() % hashTable[i].size;
-        //     hashTable[i].B = rand() % hashTable[i].size;
-        //     hashTable[i].P = generatePrime(hashTable[i].size + 1, 2 * hashTable[i].size);
-        // } while (!areCoprime(hashTable[i].P, hashTable[i].size) || !areCoprime(hashTable[i].A, hashTable[i].size));
-        assert(hashTable[i].arr);
-    }
-    // printf(":2\n");
-
-    // bool exitLoop = false;
-    for (int i = 0; i < size; i++) {
-        struct node* temp_arr = (struct node*)calloc(hashTable[i].size, sizeof(struct node));
-
-        // printf("%d ", i);
-        if (hashTable[i].size > 0) {
-                // hashTable[i].A = rand() % hashTable[i].size;
-                // hashTable[i].A += (hashTable[i].A % 2 == 0) ? 1 : 0;
-                // hashTable[i].B = rand() % hashTable[i].size;
-                // hashTable[i].P = generatePrime(hashTable[i].size + 1, 2 * hashTable[i].size);
-
-                hashTable[i].A = rand() %  hashTable[i].size;
-                hashTable[i].A = 2 * hashTable[i].A + 1;
-                hashTable[i].B = rand() %  hashTable[i].size;
-                hashTable[i].P = rand() %  hashTable[i].size +  hashTable[i].size;
-                hashTable[i].P = 2 * hashTable[i].A + 1;
-        }
-        int A2 = hashTable[i].A, B2 = hashTable[i].B, P2 = hashTable[i].P;
-
-
-        for (int qw = 0; qw < AAA[i].size; qw++) {
-            // printf("qw = %d\n", qw);
-            int key = AAA[i].arr[qw].value;
-            if (key == 0)
-                continue;
-
-            int hashValue2 = ((A2 * key + B2) % P2 + P2) % P2 % hashTable[i].size;
-
-            if (temp_arr[hashValue2].value != 0) {
-                // do {
-                    // A2 = rand() % hashTable[i].size;
-                    // A2 += (A2 % 2 == 0) ? 1 : 0;
-                    // B2 = rand() % hashTable[i].size;
-                    // P2 = generatePrime(hashTable[i].size + 1, 2 * hashTable[i].size);
-
-                    A2 = rand() % hashTable[i].size;
-                    A2  = 2 * A2 + 1;
-                    B2 = rand() % hashTable[i].size;
-                    P2 = rand() % hashTable[i].size + hashTable[i].size;
-                    P2 = 2 * P2 + 1;
-
-                for (int as = 0; as < hashTable[i].size; as++) {
-                    temp_arr[as].value = 0;
-                    temp_arr[as].flag_zero = 0;
-                }
-                qw = -1;
-            } else {
-                temp_arr[hashValue2].value = key;
-            }
-        }
-
-        hashTable[i].A = A2; hashTable[i].B = B2; hashTable[i].P = P2;
-        for (int j = 0; j < hashTable[i].size; j++) {
-            hashTable[i].arr[j].value = temp_arr[j].value;
-            hashTable[i].arr[j].flag_zero = temp_arr[j].flag_zero;
-        }
-
-        free(temp_arr);
-    }
-
-    // for (int i = 0; i < size; i++) {
-    //     printf("%d ", i);
-    //     for (int j = 0; j < hashTable[i].size; j++) {
-    //         printf("%d ", hashTable[i].arr[j].value);
-    //     }
-    //     printf("\n");
-    // }
-
-    // printf(":5\n");
-
-    char command[10];
-    scanf("%s", command);
-    while (command[0] != '.') {
-        int value = atoi(command);
-        if(value == 0)
-        {
-            if(nul) printf("YES\n");
-            else    printf("NO\n");
-                scanf("%s", command);
-            continue;
-        }
-
-
-        int hash1  = ((A * value + B) % P + P) % P % size;
-        if (hashTable[hash1].arr == NULL) {
-            printf("NO\n");
-            scanf("%s", command);
-            continue;
-        }
-
-        int A2 = hashTable[hash1].A, B2 = hashTable[hash1].B, P2= hashTable[hash1].P;
-        int hash2 = ((A2 * value + B2) % P2 + P2) % P2 % hashTable[hash1].size;
-
-        if (hashTable[hash1].arr[hash2].value == value )
-            printf("YES\n");
-        else
-            printf("NO\n");
-
-        scanf("%s", command);
-    }
-
+    free(ht_collizion);
+    free(HT);
     return 0;
 }
 
